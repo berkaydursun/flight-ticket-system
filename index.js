561f9e6e45e04ebf10f1e0e9a17f5369cf612e21
Berkay Dursun <berkay_dursun@outlook.com>
2022-01-04 11:54:07 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(3000, () => {
-    console.log('Express Server Running at ', 3000);
+app.listen(process.env.PORT, () => {
+    console.log('Express Server Running at ', process.env.PORT);
 })
